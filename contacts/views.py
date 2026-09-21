855c590a1304de646fd51acda9e0dfd557c6d776
trinath06183 <trinathnayak06183@gmail.com>
2026-09-21 14:52:52 +0530

    fix(contacts): import Decimal in contacts/views.py

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from core.decorators import require_permission
 import json
+from decimal import Decimal
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
